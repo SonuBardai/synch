ce259533695a4ad032fd41a7ef58877668edac8e
Sonu Bardai <sonubardai9@gmail.com>
2024-08-22 09:59:59 +0530

    styling updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
         <body className={font.className}>
           <ThemeProvider
             attribute="class"
-            defaultTheme="dark"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
