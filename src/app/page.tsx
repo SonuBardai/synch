c8c64e826ab931be515cd561826687f51030f570
Sonu Bardai <sonubardai9@gmail.com>
2024-08-22 20:50:09 +0530

    unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 import { CardBody, CardContainer, CardItem } from '@/components/global/3d-card';
-import { HeroParallax } from '@/components/global/connect-parallax';
 import { ContainerScroll } from '@/components/global/container-scroll-animation';
-import { InfiniteMovingCards } from '@/components/global/infinite-moving-cards';
 import { LampComponent } from '@/components/global/lamp';
 import Navbar from '@/components/global/navbar';
 import { Button } from '@/components/ui/button';
-import { clients, products } from '@/lib/constant';
 import { CheckIcon } from 'lucide-react';
 
 export default function Home() {
