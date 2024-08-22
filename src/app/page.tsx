f3cee6a8db6751a48767d3af029d8812b83fe25b
Sonu Bardai <sonubardai9@gmail.com>
2024-08-22 20:49:54 +0530

    styling fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import { CheckIcon } from 'lucide-react';
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center flex-col">
+    <main className="flex items-center justify-center flex-col pb-20">
       <Navbar />
       <section className="h-screen w-full  bg-neutral-950 rounded-md  !overflow-visible relative flex flex-col items-center  antialiased">
         <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
