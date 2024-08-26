f53e13fdeef15ebd31bd7339a6001065b41c2c22
Sonu Bardai <sonubardai9@gmail.com>
2024-08-26 10:48:59 +0530

    update theme; improve plans code

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -19,50 +19,45 @@ const MenuOptions = (props: Props) => {
   const pathName = usePathname();
 
   return (
-    <nav className=" dark:bg-black h-screen overflow-scroll  justify-between flex items-center flex-col  gap-10 py-6 px-2">
+    <nav className=" dark:bg-background h-screen overflow-scroll  justify-between flex items-center flex-col  gap-10 py-6 px-2">
       <div className="flex items-center justify-center flex-col gap-8">
         <Link className="flex font-bold flex-row " href="/">
           Synchrony
         </Link>
         <TooltipProvider>
-          {menuOptions.map((menuItem) => (
-            <ul key={menuItem.name}>
-              <Tooltip delayDuration={0}>
-                <TooltipTrigger>
-                  <li>
-                    <Link
-                      href={menuItem.href ?? '#'}
-                      className={clsx(
-                        `group h-8 w-8 flex items-center justify-center  scale-[1.5] rounded-lg p-[3px]  cursor-pointer ${
-                          (menuItem.hrefs ?? []).some((href) =>
-                            pathName.startsWith(href)
-                          )
-                            ? 'dark:bg-[#2F006B] bg-[#EEE0FF]'
-                            : ''
-                        }`
-                      )}
-                    >
-                      <menuItem.Component
-                        className={
-                          (menuItem.hrefs ?? []).some((href) =>
-                            pathName.startsWith(href)
-                          )
-                            ? 'fill-[#2F006B]'
-                            : ''
-                        }
-                      />
-                    </Link>
-                  </li>
-                </TooltipTrigger>
-                <TooltipContent
-                  side="right"
-                  className="bg-black/10 backdrop-blur-xl"
-                >
-                  <p>{menuItem.name}</p>
-                </TooltipContent>
-              </Tooltip>
-            </ul>
-          ))}
+          {menuOptions.map((menuItem) => {
+            const selected = (menuItem.hrefs ?? []).some((href) =>
+              pathName.startsWith(href)
+            );
+            return (
+              <ul key={menuItem.name}>
+                <Tooltip delayDuration={0}>
+                  <TooltipTrigger>
+                    <li>
+                      <Link
+                        href={menuItem.href ?? '#'}
+                        className={clsx(
+                          `group h-8 w-8 flex items-center justify-center  scale-[1.5] rounded-lg p-[3px]  cursor-pointer ${
+                            selected ? 'bg-[#EEE0FF]' : ''
+                          }`
+                        )}
+                      >
+                        <menuItem.Component
+                          className={selected ? 'fill-[#2F006B]' : ''}
+                        />
+                      </Link>
+                    </li>
+                  </TooltipTrigger>
+                  <TooltipContent
+                    side="right"
+                    className="bg-background/10 backdrop-blur-xl"
+                  >
+                    <p>{menuItem.name}</p>
+                  </TooltipContent>
+                </Tooltip>
+              </ul>
+            );
+          })}
         </TooltipProvider>
         <Separator />
         {/* <div className="flex items-center flex-col gap-9 dark:bg-[#353346]/30 py-4 px-2 rounded-full h-56 overflow-scroll border-[1px]">
