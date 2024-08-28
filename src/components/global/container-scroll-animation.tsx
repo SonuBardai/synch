9cc105882578b6a7f261dcd1d3aa6f749ab4646b
Sonu Bardai <sonubardai9@gmail.com>
2024-08-28 09:48:21 +0530

    add grid bg for landing page

--- src/components/global/container-scroll-animation.tsx
+++ src/components/global/container-scroll-animation.tsx
@@ -4,6 +4,18 @@ import React, { useRef } from 'react';
 import { useScroll, useTransform, motion } from 'framer-motion';
 import Image from 'next/image';
 
+export function DotBackground({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="h-[100vh] mt-36 w-full dark:bg-black bg-white dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+      {/* Radial gradient for the container to give a faded look */}
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <div className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+        {children}
+      </div>
+    </div>
+  );
+}
+
 export const ContainerScroll = ({
   titleComponent,
 }: {
@@ -35,20 +47,22 @@ export const ContainerScroll = ({
   const translate = useTransform(scrollYProgress, [0, 1], [0, -100]);
 
   return (
-    <div
-      className="h-[80rem] flex items-center justify-center relative p-20"
-      ref={containerRef}
-    >
+    <DotBackground>
       <div
-        className="py-40 w-full relative"
-        style={{
-          perspective: '1000px',
-        }}
+        className="h-[80rem] flex items-center justify-center relative p-20"
+        ref={containerRef}
       >
-        <Header translate={translate} titleComponent={titleComponent} />
-        <Card rotate={rotate} scale={scale} />
+        <div
+          className="py-40 w-full relative"
+          style={{
+            perspective: '1000px',
+          }}
+        >
+          <Header translate={translate} titleComponent={titleComponent} />
+          <Card rotate={rotate} scale={scale} />
+        </div>
       </div>
-    </div>
+    </DotBackground>
   );
 };
 
