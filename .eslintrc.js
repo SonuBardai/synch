c14d4a54ee73291d57719fb8ad48bef9e9d62459
Sonu Bardai <sonubardai9@gmail.com>
2024-08-28 10:03:18 +0530

    temp: suppress linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,6 @@ module.exports = {
     '@typescript-eslint/no-duplicate-enum-values': 'off',
     '@typescript-eslint/no-empty-object-type': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
 };
